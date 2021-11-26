ca1282e369c6ec66e9711d8024fb157914e25fa7
szymon <szymonstuszek@gmail.com>
2021-11-26 19:08:51 +0100

    displaying videos

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # ReduxSimpleStarter
 ### What is it?
 
-React project based on Stephen's Grider tutorial for building blockchain apps: </br>
+React project based on Stephen's Grider tutorial for building blockchain apps:
+Ethereum and Solidity: The Complete Developer's Guide Udemy course
+</br>
 https://github.com/StephenGrider
--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar.js';
+import VideoList from './components/video_list.js';
 
-const API_KEY = '<your-api-key>';
+const API_KEY = '';
 
 class App extends Component {
     constructor(props) {
@@ -19,6 +20,7 @@ class App extends Component {
         return (
             <div>
                 <SearchBar />
+                <VideoList videos={this.state.videos} />
             </div>
         );
     }
