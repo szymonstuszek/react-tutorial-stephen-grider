7b7cf4041a1067531fb4505ce0c3b5257b1f3fcc
szymon <szymonstuszek@gmail.com>
2021-11-26 07:31:44 +0100

    Youtube search

--- src/index.js
+++ src/index.js
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar.js';
 
 const API_KEY = '<your-api-key>';
 
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { videos: [] };
 
-const App = () => {
-    return <div>Hi!</div>;
+        YTSearch({ key: API_KEY, term: 'surfboards' }, (videos) => {
+            this.setState({ videos });
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <SearchBar />
+            </div>
+        );
+    }
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
